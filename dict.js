35b97ee4f5baab7f5d474dbb53fd6d91c7e2d98e
wds2006sdo <641614152@qq.com>
2016-07-13 23:15:20 +0800

    add meaning

--- dict.js
+++ dict.js
@@ -26,6 +26,8 @@ function add_meaning(word,obj)
 	console.log(obj);
 	
 	var meaning = get_meaning(word);
+	var meaning_obj = get_meaning_dom(obj);
+	add_meaning_in_dom(meaning_obj,meaning);
 	
 	var tmp = 'ff';
 }
@@ -33,4 +35,28 @@ function add_meaning(word,obj)
 function get_meaning(word)
 {
 	return '温德斯';
+}
+
+function get_meaning_dom(obj)
+{
+	if(obj.lastChild.tagName == undefined || obj.lastChild.className.indexOf('meaning') == -1)
+		meaning_obj = create_new_meaning_obj(obj);
+	else
+		meaning_obj = obj.lastChild;
+	
+	return meaning_obj;
+}
+
+function create_new_meaning_obj(obj)
+{
+	var insert_html = '<div class="meaning"></div>';
+	obj.innerHTML = obj.innerHTML + insert_html;
+	
+	return obj.lastChild;
+}
+
+function add_meaning_in_dom(obj,meaning)
+{
+	var insert_html = '<div class="single-meaning">' + meaning + '</div>'
+	obj.innerHTML = obj.innerHTML + insert_html;
 }
